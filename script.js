fb9723d57d944659dd7860261af79cc46b7c2d2c
Peter Oseghale Ohue <50463754+OhuePeter@users.noreply.github.com>
2023-11-24 09:07:18 +0100

    Add files via upload
    Twelve files recently added

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add JavaScript code here if needed
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Add code to handle form submission, e.g., sending data to a server
+    alert("Form submitted!");
+});
